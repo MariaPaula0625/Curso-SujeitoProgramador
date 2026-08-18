65a736c7f3ffa2b5f444f9a1886cdebd41fc014b
Maria Paula Silva <smariapaula0625@gmail.com>
2026-08-18 09:36:01 -0300

    Aula 1 sobre JavaScript

---
+++ Aulas/JavaScript/Aula01/script.js
@@ -0,0 +1,10 @@
+var nome = prompt("Qual seu nome?");
+
+var sobrenome = prompt("Qual seu sobrenome?")
+
+console.log("Olá " + nome + " seu sobrenome é " + sobrenome);
+
+document.write("<h1>Olá " + nome + " " + sobrenome + "</h1>");
+
+document.write("<img src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Steve_Jobs_Headshot_2010_%28cropped_4%29.jpg/960px-Steve_Jobs_Headshot_2010_%28cropped_4%29.jpg?utm_source=pt.wikipedia.org&utm_campaign=imageinfo&utm_content=thumbnail' alt = 'Foto de stave jobs' />");
+
