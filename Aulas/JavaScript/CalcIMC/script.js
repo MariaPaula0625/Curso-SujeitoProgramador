d5a2c302f2a0977a545f6468bc1709a2e26a74a3
Maria Paula Silva <smariapaula0625@gmail.com>
2026-08-30 17:48:33 -0300

    Praticando o JavaScript com uma calculadora IMC

---
+++ Aulas/JavaScript/CalcIMC/script.js
@@ -0,0 +1,24 @@
+var peso;
+var altura;
+var imc;
+var resultado;
+
+function calcular(event){
+    event.preventDefault();
+
+    peso = document.getElementById("peso").value;
+    altura = document.getElementById("altura").value;
+    resultado = document.getElementById("resultado");
+
+    imc = peso / (altura * altura);
+
+    if(imc < 17){
+        resultado.innerHTML = "<br/> O resultado é: " + imc + ". <br/> Você está muito abaixo do peso."
+    }else if(imc <= 18.49){
+        resultado.innerHTML = "<br/> O resultado é: " + imc + ". <br/> Você está abaixo do peso."
+    }else if(imc <= 24.99){
+        resultado.innerHTML = "<br/> O resultado é: " + imc + ". <br/> Você está com o peso normal."
+    }else{
+        resultado.innerHTML = "<br/> O resultado é: " + imc + ". <br/> Você está acima do peso."
+    }
+}
